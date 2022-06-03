ea5537a0adaa85c994e97174c3da7912bdd9ab5b
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-06-03 19:07:25 +0700

    feat: Draggable marker

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer } from 'react-leaflet'
 
 import 'leaflet/dist/leaflet.css'
 
-import DefaultIcon from 'Components/Leaflet/Marker/Default/Default'
-
 import LocationMarker from 'Components/Leaflet/Marker/Location/Location'
+import DraggableMarker from 'Components/Leaflet/Marker/Draggable/Draggable'
 
 import Minimap from 'Components/Leaflet/Minimap/Minimap'
 
@@ -25,12 +24,8 @@ const App = () => {
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 			/>
-			<Marker position={[0, 0]} icon={DefaultIcon}>
-				<Popup>
-					A pretty CSS3 popup. <br /> Easily customizable.
-				</Popup>
-			</Marker>
 			<LocationMarker />
+			<DraggableMarker />
 			<Minimap position='topright' />
 		</MapContainer>
 	)
